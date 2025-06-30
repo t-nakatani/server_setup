82f52117fea1b4d6268f5559c553dfc8ad7406e3
t-nakatani <tomoya.12@icloud.com>
2025-06-30 14:38:39 +0200

    feat: ubuntu24.04に対応

--- setup/zsh-setup.sh
+++ setup/zsh-setup.sh
@@ -5,12 +5,14 @@ sudo apt-get install -y zsh
 chsh -s $(which zsh)
 
 # Zsh 用 Git 統合スクリプトのダウンロード
-cd ~/.zsh
+mkdir -p ~/.zsh && cd ~/.zsh
 curl -o git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 curl -o git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 curl -o _git https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
 
 # Peco のインストール
-sudo apt install -y peco
-git clone https://github.com/jimeh/zsh-peco-history.git ~/.zsh/zsh-peco-history
-source ~/.zsh/zsh-peco-history/zsh-peco-history.zsh
+cd /tmp
+wget https://github.com/peco/peco/releases/download/v0.5.11/peco_linux_amd64.tar.gz
+tar -xzf peco_linux_amd64.tar.gz
+sudo mv peco_linux_amd64/peco /usr/local/bin/
+peco --version
